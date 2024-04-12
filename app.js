2fb8778df4f7fe29c400256d0bb04e52dd13660e
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-12 13:28:25 +0530

    code added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-let reqFile = '{"fact":"A cat will tremble or shiver when it is extreme pain.","length":53}';
+// let reqFile = '{"fact":"A cat will tremble or shiver when it is extreme pain.","length":53}';
 
 
 
-let validRes = JSON.parse(reqFile);
+// let validRes = JSON.parse(reqFile);
 
-console.log(validRes.fact);
+// console.log(validRes.fact);
 
-let team = {
-    cricket: "Playhard",
-    total: 123
+// let team = {
+//     cricket: "Playhard",
+//     total: 123
 
-};
+// };
+let url = "https://catfact.ninja/fact";
+
+fetch(url);
