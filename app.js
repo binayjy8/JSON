5a9d12fa92447d2da34dd5a22c24d59a9e9ab52d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-12 14:01:10 +0530

    code added

--- app.js
+++ app.js
@@ -15,11 +15,17 @@ let url = "https://catfact.ninja/fact";
 
 fetch(url)
 .then((res)=>{
-    console.log(res);
     return res.json();
 })
 .then((data)=>{
-    console.log(data);
+    console.log(data.fact);
+    return fetch(url);
+})
+.then((res)=>{
+    return res.json();
+})
+.then((data2)=>{
+    console.log("data2", data2.fact);
 })
 .catch((err)=>{
     console.log(err);
